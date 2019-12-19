8ec2a567b7a262e76f3503337a1dc5c43bd53b53
Charlie Bissell <bissell31@gmail.com>
2019-12-18 22:05:13 -0500

    added more features to api

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -40,6 +40,69 @@ namespace InventoryAPI.Controllers
       return Ok(product);
     }
 
+    [HttpPut("{id}")]
+    public ActionResult UpdateProduct(Product product)
+    {
+      var db = new DatabaseContext();
+      var prevProduct = db.Products.FirstOrDefault(p => p.Id == product.Id);
+      if (prevProduct == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        prevProduct.SKU = product.SKU;
+        prevProduct.Name = product.Name;
+        prevProduct.Description = product.Description;
+        prevProduct.NumberInStock = product.NumberInStock;
+        prevProduct.Price = product.Price;
+        db.SaveChanges();
+        return Ok(prevProduct);
+      }
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult DeleteProduct(int id)
+    {
+      var db = new DatabaseContext();
+      var product = db.Products.FirstOrDefault(p => p.Id == id);
+      if (product == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        db.Products.Remove(product);
+        db.SaveChanges();
+        return Ok("Product Deleted");
+      }
+    }
+
+    [HttpGet("OutOfStock")]
+    public ActionResult OutOfStock()
+    {
+      var db = new DatabaseContext();
+      return Ok(db.Products.Where(p => p.NumberInStock == 0));
+    }
+
+    [HttpGet("SKU/{SKU}")]
+    public ActionResult GetProductBySKU(string SKU)
+    {
+      var db = new DatabaseContext();
+      var product = db.Products.FirstOrDefault(p => p.SKU == SKU);
+      if (product == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        return Ok(product);
+      }
+
+    }
+
+
+
 
 
   }
